8c5e2f69ed5955ef98ab511bc4e36b09f704d575
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-02-03 10:57:41 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var fullName = "Cynthia Mumbua wambua"
+    println(fullName)
+
+    var age = 19
+    println(age)
+
+    var weightKg = 56
+    println(weightKg)
+
+    var phoneNumber = "0713504579"
+    println(phoneNumber)
+
+    var citizen : Boolean = true
+    println(citizen)
+}
